df7fe04994db8d58b6f88aafd42fd57de64abc59
ggh <good66591@gmail.com>
2025-02-27 17:34:35 +0900

    - test

---
+++ src/test/java/com/tansong/good665/example/JAVAtest.java
@@ -0,0 +1,10 @@
+package com.tansong.good665.example;
+
+import java.util.Scanner;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JAVAtest {
+    Scanner sc = new Scanner(System.in);
+}
