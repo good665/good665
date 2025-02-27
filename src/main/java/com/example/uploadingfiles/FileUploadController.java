34dc4b095eeda5322a87f5094ee50428e7292a4b
ggh <good66591@gmail.com>
2025-02-27 20:11:36 +0900

    spring cloud

--- src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -1,75 +1,75 @@
-package com.example.uploadingfiles;
+// package com.example.uploadingfiles;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
+// import java.io.IOException;
+// import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.core.io.Resource;
+// import org.springframework.http.HttpHeaders;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.uploadingfiles.storage.StorageFileNotFoundException;
-import com.example.uploadingfiles.storage.StorageService;
+// import com.example.uploadingfiles.storage.StorageFileNotFoundException;
+// import com.example.uploadingfiles.storage.StorageService;
 
-@Controller
-public class FileUploadController {
+// @Controller
+// public class FileUploadController {
 
-    private final StorageService storageService;
+//     private final StorageService storageService;
 
-    @Autowired
-	public FileUploadController(StorageService storageService) {
-		this.storageService = storageService;
-	}
+//     @Autowired
+// 	public FileUploadController(StorageService storageService) {
+// 		this.storageService = storageService;
+// 	}
 
-    @GetMapping("/file")
-	public String listUploadedFiles(Model model) throws IOException {
+//     @GetMapping("/file")
+// 	public String listUploadedFiles(Model model) throws IOException {
 
-		model.addAttribute("files", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
+// 		model.addAttribute("files", storageService.loadAll().map(
+// 				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+// 						"serveFile", path.getFileName().toString()).build().toUri().toString())
+// 				.collect(Collectors.toList()));
 
-		return "uploadForm";
-	}
+// 		return "uploadForm";
+// 	}
 
-    @GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//     @GetMapping("/files/{filename:.+}")
+// 	@ResponseBody
+// 	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-		Resource file = storageService.loadAsResource(filename);
+// 		Resource file = storageService.loadAsResource(filename);
 
-		if (file == null)
-			return ResponseEntity.notFound().build();
+// 		if (file == null)
+// 			return ResponseEntity.notFound().build();
 
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
+// 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+// 				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+// 	}
 
-    @PostMapping("/file")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
+//     @PostMapping("/file")
+// 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+// 			RedirectAttributes redirectAttributes) {
 
-		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
+// 		storageService.store(file);
+// 		redirectAttributes.addFlashAttribute("message",
+// 				"You successfully uploaded " + file.getOriginalFilename() + "!");
 
-		return "redirect:/";
-	}
+// 		return "redirect:/";
+// 	}
 
-    @ExceptionHandler(StorageFileNotFoundException.class)
-	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-		return ResponseEntity.notFound().build();
-	}
+//     @ExceptionHandler(StorageFileNotFoundException.class)
+// 	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+// 		return ResponseEntity.notFound().build();
+// 	}
     
-}
+// }
--- src/main/java/com/gateway/Application.java
+++ src/main/java/com/gateway/Application.java
@@ -3,16 +3,29 @@ package com.gateway;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import reactor.core.publisher.Mono;
 
 public class Application {
 
-@Bean
-public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-    return builder.routes()
-        .route(p -> p
-            .path("/get")
-            .filters(f -> f.addRequestHeader("Hello", "World"))
-            .uri("http://httpbin.org:80"))
-        .build();
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(p -> p
+                .path("/get")
+                .filters(f -> f.addRequestHeader("Hello", "World"))
+                .uri("http://httpbin.org:80"))
+            .route(p -> p
+                .host("*.circuitbreaker.com")
+                .filters(f -> f.circuitBreaker(config -> config
+                    .setName("mycmd")
+                    .setFallbackUri("forward:/fallback")))
+                .uri("http://httpbin.org:80"))
+            .build();
+    }
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
 }
 }
