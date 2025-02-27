6bc6952d3856f4ac87b3e04512a132ea12d35b7f
ggh <good66591@gmail.com>
2025-02-27 10:07:00 +0900

    spring boot test

---
+++ src/test/java/com/tansong/good665/ConsumingRestApplicationTest.java
@@ -0,0 +1,25 @@
+package com.tansong.good665;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ConsumingRestApplicationTest {
+
+	// @Autowired
+	// private RestTemplate restTemplate;
+
+	// @Test
+	// public void contextLoads() {
+	// 	assertThat(restTemplate).isNotNull();
+	// }
+
+}
---
+++ src/test/java/com/tansong/good665/TanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tansong.good665;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
