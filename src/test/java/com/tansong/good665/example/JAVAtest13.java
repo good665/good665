81bc872e33a88a4679808674ad61913bbca4c002
guihan <ggh-ucert@gmail.com>
2025-03-06 09:50:45 +0900

    -stduy

---
+++ src/test/java/com/tansong/good665/example/JAVAtest13.java
@@ -0,0 +1,24 @@
+package com.tansong.good665.example;
+
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JAVAtest13 {
+    @Test
+	public void contextLoads2_1181() {
+        
+    }
+
+    @Test
+	public void contextLoads2_10814() {
+        
+    }
+
+    @Test
+	public void contextLoads2_18870() {
+        
+    }
+}
